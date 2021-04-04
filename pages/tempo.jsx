d5d60777e737f784068d1d68c096f926e96275cd
David Bastos <davidtavares98@gmail.com>
2021-04-04 12:23:12 -0300

    staticDateString

--- pages/tempo.jsx
+++ pages/tempo.jsx
@@ -1,12 +1,25 @@
-function Tempo() {
+function Tempo({ staticDateString }) {
   const dynamicDate = new Date();
   const dynamicDateString = dynamicDate.toGMTString();
 
   return (
-      <div>
-          {dynamicDateString}(Dinâmico)
-      </div>
-  )
+    <>
+      <div>{dynamicDateString}(Dinâmico)</div>
+      <div>{staticDateString}(Estático)</div>
+    </>
+  );
+}
+
+// Na disso vai para o backend
+export function getStaticProps() {
+  const staticDate = new Date();
+  const staticDateString = staticDate.toGMTString();
+
+  return {
+    props: {
+      staticDateString,
+    },
+  };
 }
 
 export default Tempo;
