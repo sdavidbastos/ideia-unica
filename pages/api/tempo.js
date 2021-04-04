c584e313986639066eec6ce686a7d32e82c517fa
David Bastos <davidtavares98@gmail.com>
2021-04-04 18:15:39 -0300

    setHeader

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,20 @@
-function tempo(request, response) {
-  const dynamicDate = new Date();
+async function tempo(request, response) {
+    const apiSecret = process.env.SECRET_API_KEY;
+    const dynamicDate = new Date();
 
-  response.json({
-    date: dynamicDate.toGMTString(),
-  });
+    const productsResponse = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+    );
+    const productsResponseJson = await productsResponse.json();
+
+    const { username } = productsResponseJson[0];
+
+    response.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
+
+    response.json({
+        date: dynamicDate.toGMTString(),
+        username,
+    });
 }
 
 export default tempo;
